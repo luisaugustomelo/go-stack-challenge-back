bf177435f7ed986fde138bae14f62fc4200c4100
Luis Rohten <luisaugustomelo@gmail.com>
2021-01-21 14:05:09 -0300

    modify description about the transaction method's

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -9,18 +9,28 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   // TODO
+  response.json({status: true});
 });
 
+/**
+ * POST /transactions: A rota deve receber title, value, type, e category dentro do corpo da requisição, sendo o type o tipo da transação,
+ * que deve ser income para entradas (depósitos) e outcome para saídas (retiradas). Ao cadastrar uma nova transação, ela deve ser armazenada
+ * dentro do seu banco de dados, possuindo os campos id, title, value, type, category_id, created_at, updated_at.
+ */
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  response.json({status: true});
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  response.json({status: true});
 });
 
 transactionsRouter.post('/import', async (request, response) => {
   // TODO
+  response.json({status: true});
 });
 
 export default transactionsRouter;
